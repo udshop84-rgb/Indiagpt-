c8a7559c32815fddd594862d1ebcb693b0133f46
Index.html <316563529+udshop84-rgb@users.noreply.github.com>
2026-09-11 11:14:14 +0530

    auth.js

---
+++ auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      error: 'Authentication required. No token provided.'
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        error: 'Invalid or expired session token.'
+      });
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = {
+  authenticateToken
+};
